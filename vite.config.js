9fcd1c89eb96d1540951ef7c3bd9d21ce4b5019d
wangzhongjie <wangzj@cyberpeace.cn>
2021-07-02 10:03:11 +0800

    config🔧:  一些配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 const { createVuePlugin } = require('vite-plugin-vue2')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()]
+  plugins: [createVuePlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    }
+  },
+  server: {
+    open: true
+  }
 })
