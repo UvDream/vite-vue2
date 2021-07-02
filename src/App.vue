5564b2dfefa324b03e92d3d26fab640d19aa3c79
wangzhongjie <wangzj@cyberpeace.cn>
2021-07-02 09:37:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {},
+  components: {},
+};
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: wangzhongjie
+ * @Date: 2021-07-02 09:25:11
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2021-07-02 09:26:31
+ * @Description: home
+ * @Email: UvDream@163.com
+-->
+<template>
+  <div class="home">home</div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {},
+  components: {},
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  outline: 1px solid red;
+}
+</style>
